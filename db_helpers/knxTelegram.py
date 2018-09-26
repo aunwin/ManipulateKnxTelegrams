cd3fee047cd5b6b4098aa2a76579c052e2f2a7e1
Johannes Goltz <johannes.goltz@uni-rostock.de>
2018-09-26 13:56:18 +0200

    validation check added + refactoring
    - added validation-function to check cemi and other values in database
    - moved properties of telegram in dic to have them iterable

--- db_helpers/knxTelegram.py
+++ db_helpers/knxTelegram.py
@@ -1,15 +1,17 @@
-class KnxTelegram:
-    sequence_number = None  # Not interesting - not included in cEMI
-    timestamp = None  # Not interesting - not included in cEMI
-    source_addr = None  # Byte 4:6
-    destination_addr = None  # Byte 6:8
-    apci = None  # Byte 9: (Bits 6:)
-    tpci = None  # Byte 9: (Bits 0:6/8)
-    priority = None  # Byte 2 (Bits 4:6)
-    repeated = None  # Byte 2 (Bit 2:3)
-    hop_count = None  # Byte 3 (Bits 1:4)
-    apdu = None  # Bytes 9:
-    payload_length = None  # Byte 8:9
-    cemi = None  # value to be calculated/checked
-    is_manipulated = None  # Not interesting - not included in cEMI
-    attack_type_id = None  # Not interesting - not included in cEMI
+class KnxTelegram(object):
+    properties = {
+        "sequence_number": None,
+        "timestamp": None,
+        "source_addr": None,
+        "destination_addr": None,
+        "apci": None,
+        "tpci": None,
+        "priority": None,
+        "repeated": None,
+        "hop_count": None,
+        "apdu": None,
+        "payload_length": None,
+        "cemi": None,
+        "is_manipulated": None,
+        "attack_type_id": None
+    }
--- validate_knx_log.py
+++ validate_knx_log.py
@@ -1,16 +1,86 @@
 #!/usr/bin/env python3
 
 from db_helpers import db_acces as db
+from db_helpers import knxTelegram as dbKnxTelegram
 from config import dbconfig
+import binascii
+
+import baos_knx_parser as knx
 
 
 connection, cursor = db.init_db_connections(dbconfig)
 
-sql_statement = f'SELECT * FROM {dbconfig.knx_log_db["table"]} LIMIT 10;'
+sql_statement = f'SELECT * FROM {dbconfig.knx_log_db["table"]} LIMIT 1;'
 
 cursor.execute(sql_statement)
 
+telegram_diviations = 0
 for row in cursor:
-    print(row)
+    dbTelegram = dbKnxTelegram.KnxTelegram
+    i = 0
+    for dbItem in dbTelegram.properties:
+        dbTelegram.properties[dbItem] = row[i]
+        i += 1
+
+    for dbItem in dbTelegram.properties:
+        print(str(dbItem) + ": " + str(dbTelegram.properties[dbItem]))
+
+    parsedTelegram = knx.parse_knx_telegram(binascii.a2b_hex(dbTelegram.properties['cemi']))
+
+    propertie_deviations = 0
+
+    if dbTelegram.properties["source_addr"] != str(parsedTelegram.src):
+        print(f'Error in src-Property: dbValue: {dbTelegram.properties["source_addr"]} - '
+              f'parsedValue: {parsedTelegram.src}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["destination_addr"] != str(parsedTelegram.dest):
+        print(f'Error in dest-Property: dbValue: {dbTelegram.properties["destination_addr"]} - '
+              f'parsedValue: {parsedTelegram.dest}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["apci"] != str(parsedTelegram.apci):
+        print(f'Error in apci-Property: dbValue: {dbTelegram.properties["apci"]} - '
+              f'parsedValue: {parsedTelegram.apci}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["tpci"] != str(parsedTelegram.tpci):
+        print(f'Error in tpci-Property: dbValue: {dbTelegram.properties["tpci"]} - '
+              f'parsedValue: {parsedTelegram.tpci}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["priority"] != str(parsedTelegram.priority):
+        print(f'Error in priority-Property: dbValue: {dbTelegram.properties["priority"]} - '
+              f'parsedValue: {parsedTelegram.priority}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["repeated"] != parsedTelegram.repeat:
+        print(f'Error in repeated-Property: dbValue: {dbTelegram.properties["repeated"]} - '
+              f'parsedValue: {parsedTelegram.repeat}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["hop_count"] != parsedTelegram.hop_count:
+        print(f'Error in hop_count-Property: dbValue: {dbTelegram.properties["hop_count"]} - '
+              f'parsedValue: {parsedTelegram.hop_count}')
+        propertie_deviations += 1
+
+    if binascii.a2b_hex(dbTelegram.properties["apdu"]) != parsedTelegram.payload:
+        print(f'Error in apdu-Property: dbValue: {dbTelegram.properties["apdu"]} - '
+              f'parsedValue: {parsedTelegram.payload}')
+        propertie_deviations += 1
+
+    if dbTelegram.properties["payload_length"] != parsedTelegram.payload_length:
+        print(f'Error in payload_length-Property: dbValue: {dbTelegram.properties["payload_length"]} - '
+              f'parsedValue: {parsedTelegram.payload_length}')
+        propertie_deviations += 1
+
+    if propertie_deviations:
+        print(f'WARNING: This telegram contains {propertie_deviations} property-missmatches')
+        telegram_diviations += 1
+    # print(parsedTelegram.__dict__)
+    # print(parsedTelegram)
+
+if telegram_diviations:
+    print(f'WARNING: This set of telegrams contains {telegram_diviations} invalid telegrams!')
 
 db.close_db_connection(connection, cursor)
