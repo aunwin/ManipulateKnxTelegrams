58d2fbb382c028ca27428d0d6bdf3d68f5a76b23
Johannes Goltz <johannes.goltz@uni-rostock.de>
2018-09-26 08:04:50 +0200

    refactoring
    - changed sources to be PEP-8-conform

--- validate_knx_log.py
+++ validate_knx_log.py
@@ -3,6 +3,7 @@
 from db_helpers import db_acces as db
 from config import dbconfig
 
+
 connection, cursor = db.init_db_connections(dbconfig)
 
 sql_statement = f'SELECT * FROM {dbconfig.knx_log_db["table"]} LIMIT 10;'
@@ -13,6 +14,3 @@ for row in cursor:
     print(row)
 
 db.close_db_connection(connection, cursor)
-
-
-
