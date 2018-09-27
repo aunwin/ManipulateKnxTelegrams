7d4e296058c03e5398f00c2f591b5bbe457575f0
Simeon Wiedenmann <simeon.wiedenmann@uni-rostock.de>
2018-09-27 17:38:49 +0200

    set_hop_count, get_daf, replace_nipple, parse_add_length
    implements the following functions
        set_hop_count(calue, cemi)
        get_destination_address_flag(cemi)
        replace_nipple(position, value, cemi)   !UGLY - NEEDS IMPROVEMENT
        parse_length_of_additional_info(cemi)   !UGLY - NEEDS IMPROVEMENT

--- update_cemi.py
+++ update_cemi.py
@@ -3,7 +3,34 @@ import baos_knx_parser as knx
 import binascii
 
 
-def setHopCount(value, cemi):
-    parsedTelegram = knx.parse_knx_telegram(binascii.a2b_hex(cemi))
-    print(parsedTelegram.to_binary())
+def set_hop_count(value, cemi):
+    if value > 7 | value < 0:
+        raise OverflowError('No hopecount smaler than 0 or larger than 7 allowed!')
+    hop = (value)
+    daf = (get_destintation_address_flag(cemi) << 3)
+    hopcnt_daf_nipple = (hop | daf)
+
+    additional_length = parse_length_of_additional_info(cemi)
+
+    position = 6 + additional_length * 2    # 2 nipple pro additional Byte
+
+    cemi = replace_nipple(position, hopcnt_daf_nipple, cemi)
+
     return cemi
+
+def get_destintation_address_flag(cemi):
+    parsed_telegram = knx.parse_knx_telegram(binascii.a2b_hex(cemi))
+    return parsed_telegram.dest.is_group_address()
+
+def replace_nipple(position, value, cemi):
+    #todo get this crap done propperly! Ugly code will hunt you down! Zoombies are comming for ugly coders first! *uargh*
+    mutable_cemi = bytearray(cemi)
+    mutable_cemi[position] = ord((str(hex(value))[2]).upper()) #turn integer into hex = '0x?' -> Cut off '0x' -> capitalize Letter
+    # possible alternative way might be something along the lines of
+    #new_cemi = cemi[0:2] + bytes(int('1', 16)) + cemi[3:]
+
+    cemi = (bytes(mutable_cemi))
+    return cemi
+
+def parse_length_of_additional_info(cemi):
+    return int(cemi[2:4], 16)
