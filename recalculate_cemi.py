a8bb79df901b67c8cef2db20f1bee84f77184aea
Simeon Wiedenmann <simeon.wiedenmann@uni-rostock.de>
2018-09-28 16:43:12 +0200

    adds set_src_address(src_addr,cemi) functionality
    - set_hop_count now throws AssertionError

--- recalculate_cemi.py
+++ recalculate_cemi.py
@@ -5,7 +5,7 @@ import binascii
 
 def set_hop_count(value, cemi):
     if value > 7 | value < 0:
-        raise OverflowError('No hopecount smaler than 0 or larger than 7 allowed!')
+        raise AssertionError('No hopecount smaler than 0 or larger than 7 allowed!')
     hop = (value)
     daf = (get_destintation_address_flag(cemi) << 3)
     hopcnt_daf_nipple = (hop | daf)
@@ -19,23 +19,30 @@ def set_hop_count(value, cemi):
     return cemi
 
 #todo WIP - do not use this function
-def set_src_address(value, cemi):
-    #todo validation check for value missing
-    #if value > 7 | value < 0:
-    #    raise OverflowError('No hopecount smaler than 0 or larger than 7 allowed!')
-    print(value)
-    src_address_bytes = value
+def set_src_address(src_address, cemi):
+
+    area, line, device = src_address.split('.')
+    area = int(area)
+    line = int(line)
+    device = int(device)
+
+    if area > 15 | line > 15 | device > 255 | area < 0 | line < 0 | device < 0:
+        raise AssertionError(f'Invalid src_address. Area = ({area}) or Line = ({line}) or Device = ({device}) value out of bounds!')
+
+    device1 = device // 16
+    device2 = device % 16
 
     additional_length = parse_length_of_additional_info(cemi)
     position = 8 + additional_length * 2    # 2 Nipple pro additional Byte
 
-    mutable_cemi = bytearray(cemi)
-    print(mutable_cemi[position:position+4])
+    new_cemi = bytearray(cemi)
 
-    #cemi = replace_nipple(position, src_address_bytes, cemi)
+    new_cemi[position] = ord((str(hex(area))[2]).upper()) #turn integer into hex = '0x?' -> Cut off '0x' -> capitalize Letter
+    new_cemi[position+1] = ord((str(hex(line))[2]).upper()) #turn integer into hex = '0x?' -> Cut off '0x' -> capitalize Letter
+    new_cemi[position+2] = ord((str(hex(device1))[2]).upper()) #turn integer into hex = '0x?' -> Cut off '0x' -> capitalize Letter
+    new_cemi[position+3] = ord((str(hex(device2))[2]).upper()) #turn integer into hex = '0x?' -> Cut off '0x' -> capitalize Letter
 
-    raise NotImplementedError
-    return cemi
+    return bytes(new_cemi)
 
 def get_destintation_address_flag(cemi):
     parsed_telegram = knx.parse_knx_telegram(binascii.a2b_hex(cemi))
