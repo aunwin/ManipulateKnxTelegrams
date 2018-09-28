d65d35ba0806eaee6eb8560f0e7a31be32cf28e6
Johannes Goltz <johannes.goltz@uni-rostock.de>
2018-09-28 14:41:59 +0200

    refactoring get_telegram
    - returning now list of db_helpers.knxTelegram.KnxTelegrams instead of
    raw rows out of db

--- db_helpers/db_get_telegram.py
+++ db_helpers/db_get_telegram.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from db_helpers import db_update
+from db_helpers import knxTelegram
 
 
 def get_db_telegram(db_config, cursor, primary_key_value):
@@ -15,8 +16,24 @@ def get_db_telegram(db_config, cursor, primary_key_value):
         print(f'Error while getting entry from database: {err}')
         raise Exception('Error while getting entry from database: %s', err)
 
-    result_set = []
+    result_set = [knxTelegram.KnxTelegram]
     for row in cursor:
-        result_set.append(row)
+        temp = knxTelegram.KnxTelegram()
+        temp.properties['sequence_number'] = row[0]
+        temp.properties['timestamp'] = row[1]
+        temp.properties['source_addr'] = row[2]
+        temp.properties['destination_addr'] = row[3]
+        temp.properties['apci'] = row[4]
+        temp.properties['tpci'] = row[5]
+        temp.properties['priority'] = row[6]
+        temp.properties['repeated'] = row[7]
+        temp.properties['hop_count'] = row[8]
+        temp.properties['apdu'] = row[9]
+        temp.properties['payload_length'] = row[10]
+        temp.properties['cemi'] = row[11]
+        temp.properties['is_manipulated'] = row[12]
+        temp.properties['attack_type_id'] = row[13]
+
+        result_set.append(temp)
 
     return result_set
